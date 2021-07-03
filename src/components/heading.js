b540e7176a2eec6b23817f240bee3761f53cd414
felixkwan <felixkwan101@gmail.com>
2021-07-03 18:51:53 +0800

    new page

---
+++ src/components/heading.js
@@ -0,0 +1,26 @@
+import * as React from "react"
+import PropTypes from "prop-types"
+
+const Heading = () => {
+  <div>
+      <section className="heading">
+      <div className="staggerLinges">
+       <h1>Long Term</h1><span>888</span>
+      </div>
+      <div className="staggerLines">
+        <span>Lifetime</span><h1>Stability</h1>
+      </div>
+      <button>
+          I'm button.
+      </button>
+      <div className="staggerLines">
+        <h3>See all in for the</h3>
+      </div>
+      <div className="staggerLines">
+        <h3 className="highlight">#modern</h3><h3>feel</h3>
+      </div>
+    </section>
+  </div>
+}
+
+export default Heading
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
 import * as React from "react"
 import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
+import Heading from "../components/heading"
 import Subscribe from "../components/subscribe"
 import About from "../components/about"
 import Seo from "../components/seo"
@@ -11,27 +11,9 @@ import "tailwindcss/tailwind.css"
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <section className="heading">
-      <div className="staggerLinges">
-       <h1>Long Term</h1><span>888</span>
-      </div>
-      <div className="staggerLines">
-        <span>Lifetime</span><h1>Stability</h1>
-      </div>
-      <button>
-        <Link></Link>
-      </button>
-      <div className="staggerLines">
-        <h3>See all in for the</h3>
-      </div>
-      <div className="staggerLines">
-        <h3 className="highlight">#modern</h3><h3>feel</h3>
-      </div>
-    </section>
-
+    
+    <Heading/>
     <About/>
-    <Subscribe/>
-
 
   </Layout>
 )
